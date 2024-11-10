15dec800f7d39de38a6ee0632bfa6833504941a5
kaustav234 <kaustavgupta2311@gmail.com>
2024-11-10 20:20:58 +0530

    updating week 2 easy question

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -14,8 +14,21 @@
 // if str1 == str2 ==> true
 // else => false
 
-function isAnagram(str1, str2) {
 
+function isAnagram(str1, str2) {
+  const sortedString1 = str1.toLowerCase().split("").sort().join("");
+// short way of writing the program instead of giving 4 variables 
+
+  const lowerCaseString2 = str2.toLowerCase();
+  const arr2 = lowerCaseString2.split("") // kaus => ["k", "a", "u", "s"]
+  arr2.sort(); // ["a", "k", "u"]
+  const sortedString2 = arr2.join(""); // original string sorted in lower case
+  if(sortedString1 == sortedString2) {
+    return true;
+  }
+  return false;
 }
 
+
 module.exports = isAnagram;
+
